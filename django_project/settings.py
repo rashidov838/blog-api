cd9e92127071e496a2eb13b6ceb27886bfea4a50
rashidov838 <bekzodrashidov73@gmail.com>
2024-04-15 21:09:14 +0500

    Deployment checklist

--- django_project/settings.py
+++ django_project/settings.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from environs import Env
 env= Env()
 env.read_env()
+DATABASES = {
+    "default": env.dj_db_url("DATABASE_URL") 
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8&tg@77ss%%65h4qxxjoq6my59dv2el08saoy!d!zat!&2x%t&'
-
+# SECRET_KEY = 'django-insecure-8&tg@77ss%%65h4qxxjoq6my59dv2el08saoy!d!zat!&2x%t&'
+SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =[".herokuapp.com", "localhost", "127.0.0.1"] # new
 
 
 # Application definition
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount", # new
     "dj_rest_auth.registration", # new
     "drf_spectacular", # new
+    "whitenoise.runserver_nostatic", # new
     # Local
     "accounts.apps.AccountsConfig", # new
     "posts.apps.PostsConfig", # new
@@ -46,6 +50,7 @@ AUTH_USER_MODEL = "accounts.CustomUser" # new
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # new
     "corsheaders.middleware.CorsMiddleware", # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -99,12 +104,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -141,8 +146,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATICFILES_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage" # new
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
